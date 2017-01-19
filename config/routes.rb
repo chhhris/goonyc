8cff59a443515a3448378b5603be27417c27a49d
Chris Lake <chris.lake@ellucian.com>
2017-01-19 02:15:09 -0800

    About route

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get 'contact', to: 'trips#contact'
   get '/oj', to: 'trips#oj'
   get '/trips', to: 'trips#index'
+  get '/about', to: 'trips#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
