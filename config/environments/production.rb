a094658964ed5c7a667accfdef46a074b4e84bc9
Chris Lake <chris.lake@ellucian.com>
2017-01-23 00:14:20 -0800

    Background img working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  config.action_controller.asset_host = ENV['ASSET_HOST']
+  # config.action_controller.asset_host = ENV['ASSET_HOST']
   config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
   config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
 
