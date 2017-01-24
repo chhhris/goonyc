6e11da928d78ed1001789484127fd05f5ba8f6dd
Chris Lake <chris.lake@ellucian.com>
2017-01-23 17:02:46 -0800

    Date formatter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def format_date(depart_at: depart_at, return_at: return_at)
+    departure_date = "#{date.strftime('%B').downcase} #{date.strftime('%e').downcase}"
+    return_date = trip.return_at.strftime('%e')
+    if trip.return_at.strftime('%B') != trip.depart_at.strftime('%B')
+      return_date.prepend("#{trip.return_at.strftime('%B').downcase} ")
+    end
+    return "#{departure_date}-#{return_date}"
+  end
 end
