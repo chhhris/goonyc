c29c145cb6c8fdbf16f55b6e3431434c663aa9f4
Christopher Lake <mr.lake@gmail.com>
2017-10-03 22:48:27 -0700

    Belize

--- app/models/concerns/trip_destinations.rb
+++ app/models/concerns/trip_destinations.rb
@@ -1,8 +1,18 @@
 module TripDestinations
   extend ActiveSupport::Concern
 
+  # check state/country results
+  # http://api.wunderground.com/api/37f2fcd1fbfbe9b0/conditions/q/AT.json
+
+  # API call
+  # p_depart = t.depart_at.strftime('%m%d')
+  # p_return = t.return_at.strftime('%m%d')
+  # destination = Trip::DESTINATION_NAME_MAPPING[:BZE][:weather_lookup]
+  # http://api.wunderground.com/api/#{Trip::WEATHER_API_KEY}/planner_#{p_depart}#{p_return}/q/#{destination}.json
+
   DESTINATION_NAME_MAPPING = {
     ANU: { name: 'Antigua', weather_lookup: 'AT' },
+    BZE: { name: 'Belize', weather_lookup: 'BZE' },
     CUN: { name: 'Tulum', weather_lookup: 'QR/Cancun_International' },
     STT: { name: 'Saint Thomas', weather_lookup: 'STT' },
     POS: { name: 'Port of Spain', weather_lookup: 'TT/Piarco' }
