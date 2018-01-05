d489516ce257e4294657c81d8541569cf934db7f
Christopher Lake <mr.lake@gmail.com>
2018-01-05 12:49:25 -0800

    Refactor flights worker

--- app/workers/flights_worker.rb
+++ app/workers/flights_worker.rb
@@ -25,26 +25,26 @@ class FlightsWorker
 
   def get(polling_url, params)
     RestClient.get("#{polling_url}?apiKey=#{Trip::FLIGHT_API_KEY}", { accept: :json }) do |response, request, result, &block|
-      puts "GET response.code #{response.code}"
+      puts "#### GET response.code #{response.code}"
       return unless response.code == 200
       cheapest_flight = JSON.parse(response)['Itineraries'].first
 
       if cheapest_flight.present?
-        flight_price = cheapest_flight['PricingOptions'].first['Price']
-        booking_link = cheapest_flight['PricingOptions'].first['DeeplinkUrl']
+        puts "#### params #{params}"
+        pricing_options = cheapest_flight['PricingOptions'].first
         params = params.with_indifferent_access
-        code = params['destinationplace']
-        depart_at = params['outbounddate']
-        return_at = params['inbounddate']
 
-        puts "#### saving trip #### params flight worker #{params}"
-        trip = Trip.where(code: code, depart_at: depart_at.to_date, return_at: return_at.to_date).first_or_initialize
+        trip = Trip.where(
+          code: params['destinationplace'],
+          depart_at: params['outbounddate'].to_date,
+          return_at: params['inbounddate'].to_date
+        ).first_or_initialize
         trip.name = Trip::DESTINATION_NAME_MAPPING[code.to_sym][:name]
-        trip.price = flight_price
-        trip.url = booking_link
+        trip.price = pricing_options['Price']
+        trip.url = pricing_options['DeeplinkUrl']
+
         if trip.save
-          p_depart = trip.depart_at.strftime('%m%d')
-          p_return = trip.return_at.strftime('%m%d')
+          p_depart, p_return = trip.depart_at.strftime('%m%d'), trip.return_at.strftime('%m%d')
           destination = Trip::DESTINATION_NAME_MAPPING[trip.code.to_sym][:weather_lookup]
           url = "http://api.wunderground.com/api/#{Trip::WEATHER_API_KEY}/planner_#{p_depart}#{p_return}/q/#{destination}.json"
           TemperaturesWorker.perform_async(url, trip.id)
