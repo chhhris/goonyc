c06458db689c54aacd295ab9effc33bf3a0337d1
Christopher Lake <mr.lake@gmail.com>
2018-01-31 16:20:31 -0800

    3 new destinations

--- app/models/concerns/trip_destinations.rb
+++ app/models/concerns/trip_destinations.rb
@@ -10,6 +10,9 @@ module TripDestinations
   # s = Trip::DESTINATION_NAME_MAPPING[:BZE][:weather_lookup]
   # http://api.wunderground.com/api/#{Trip::WEATHER_API_KEY}/planner_#{p_depart}#{p_return}/q/#{destination}.json
 
+  # upload image to s3:
+  # aws s3 cp app/assets/images/XXX.jpg s3://getoutofnyc/XXX.jpg --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --profile chhhris
+
   DESTINATION_NAME_MAPPING = {
     ANU: { name: 'Antigua', weather_lookup: 'AT' },
     BGI: { name: 'Barbados', weather_lookup: 'BGI' },
@@ -21,7 +24,12 @@ module TripDestinations
     PTP: { name: 'Guadalupe', weather_lookup: 'PTP' },
     PSE: { name: 'Ponce, Puerto Rico', weather_lookup: 'PSE' },
     PLS: { name: 'Turks and Caicos', weather_lookup: 'PLS' },
-    SDQ: { name: 'Santo Domingo, Dominican Republic', weather_lookup: 'SDQ' }
-    # ECP: { name: 'Panama City', weather_lookup: 'ECP' }
+    SDQ: { name: 'Santo Domingo, Dominican Republic', weather_lookup: 'SDQ' },
+    AUA: { name: 'Aruba', weather_lookup: 'AW/AUA' },
+    PUJ: { name: 'Punta Cana', weather_lookup: 'PUJ' },
+    SXM: { name: 'Sint Maarten', weather_lookup: 'SXM' },
+    ECP: { name: 'Panama City', weather_lookup: 'ECP' }
   }
 end
+
+# aws s3 cp app/assets/images/sxm.jpg s3://getoutofnyc/sxm.jpg --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --profile chhhris
